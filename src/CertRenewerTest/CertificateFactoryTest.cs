affc4b6a99343c8500ddfe383667905917a3c8d1
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-02-22 00:32:40 -0500

    tests: add missing CertRenewer tests

---
+++ src/CertRenewerTest/CertificateFactoryTest.cs
@@ -0,0 +1,34 @@
+using System.Security.Cryptography.X509Certificates;
+using CertRenewer;
+
+namespace CertRenewerTest;
+
+[TestClass]
+public class CertificateFactoryTest
+{
+	private readonly X509Certificate2 TestCertificate;
+	public CertificateFactoryTest()
+	{
+		CertificateFactory.tmp();
+		TestCertificate = new X509Certificate2(
+			GetCertificatePath(
+				"TestCACertificate.pfx"
+			),
+			(string?)null,
+			X509KeyStorageFlags.Exportable | X509KeyStorageFlags.EphemeralKeySet
+		);
+	}
+
+	private static string GetCertificatePath(string FileName) => Path.Combine(
+		AppDomain.CurrentDomain.BaseDirectory,
+		"../../../../../TestCertificates",
+		FileName
+	);
+
+	[TestMethod]
+	public void RenewCertificate()
+	{
+		using var newCert = CertificateFactory.RenewCertificate(TestCertificate, "CN=test", DateTimeOffset.UtcNow.AddDays(12));
+		Assert.AreEqual("CN=test", newCert.Subject);
+	}
+}
