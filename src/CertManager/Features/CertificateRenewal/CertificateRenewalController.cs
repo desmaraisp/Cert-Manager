336130489d1e6203a8cbfac5752f8ae0849d79a7
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-02-23 08:45:30 -0500

    fix: disallow creating certRenewalSchedule with invalid cert CN

--- src/CertManager/Features/CertificateRenewal/CertificateRenewalController.cs
+++ src/CertManager/Features/CertificateRenewal/CertificateRenewalController.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using CertManager.Features.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +12,12 @@ namespace CertManager.Features.CertificateRenewal;
 public class CertificateRenewalController : ControllerBase
 {
 	private readonly CertificateRenewalService renewalService;
+	private readonly ILogger<CertificateRenewalController> logger;
 
-	public CertificateRenewalController(CertificateRenewalService renewalService)
+	public CertificateRenewalController(CertificateRenewalService renewalService, ILogger<CertificateRenewalController> logger)
 	{
 		this.renewalService = renewalService;
+		this.logger = logger;
 	}
 
 	[HttpGet("CertificateRenewalSchedules", Name = nameof(GetCertificateRenewalSchedules))]
@@ -53,6 +57,15 @@ public class CertificateRenewalController : ControllerBase
 	[RequiredScope(AuthenticationScopes.WriteScope)]
 	public async Task<IActionResult> CreateCertificateRenewalSubscriptions(CertificateRenewalSubscriptionModel Payload)
 	{
+		try
+		{
+			_ = new X500DistinguishedName(Payload.CertificateSubject);
+		}
+		catch (CryptographicException e)
+		{
+			logger.LogDebug(e, "{x} isn't a valid certificate subject", Payload.CertificateSubject);
+			return BadRequest("Certificate subject isn't valid");
+		}
 		var createdItem = await renewalService.CreateRenewalSubscription(Payload);
 
 		return Ok(createdItem);
