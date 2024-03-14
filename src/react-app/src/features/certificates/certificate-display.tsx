b0432081c474f8bf41f6097c5500f3a7bcbd68b6
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-03-14 18:25:12 -0400

    feat(frontend): add delete button to cert display

--- src/react-app/src/features/certificates/certificate-display.tsx
+++ src/react-app/src/features/certificates/certificate-display.tsx
@@ -3,9 +3,10 @@ import { Card, Stack, Box, LoadingOverlay, Text, Flex, Pill, Group } from "@mant
 import { z } from "zod"
 import { schemas } from "../../generated/client"
 import { useAuthHelperForceAuthenticated } from "../authentication/use-auth-helper"
+import { DeleteButton } from "./delete-button"
 
 export function CertificateDisplay({ certificateId, organizationId }: { certificateId: string, organizationId: string }) {
-	const {bearerToken} = useAuthHelperForceAuthenticated()
+	const { bearerToken } = useAuthHelperForceAuthenticated()
 
 	const { data, isFetching } = hooks.useGetCertificateById({
 		params: { organizationId: organizationId, id: certificateId },
@@ -21,22 +22,25 @@ export function CertificateDisplay({ certificateId, organizationId }: { certific
 				overlayProps={{ radius: 'sm', blur: 2 }}
 				loaderProps={{ color: 'pink', type: 'bars' }}
 			/>
-			{(!data) ? <Text>No data yet</Text> : <InternalCertificateDisplay data={data} />}
+			{(!data) ? <Text>No data yet</Text> : <InternalCertificateDisplay data={data} organizationId={organizationId} />}
 		</Box>
 	</Card>
 }
 
-function InternalCertificateDisplay({ data }: { data: z.infer<typeof schemas.CertificateModelWithId> }) {
-	return <Stack>
-		<Group justify="left">
-			<Text>Certificate name</Text>
-			<Text>{data.certificateName}</Text>
-			<Flex wrap={"wrap"} style={{ maxWidth: 150 }}>
-				{data.tags?.map(y => <Pill key={y}>{y}</Pill>)}
-			</Flex>
-		</Group>
-		<Text>{`Certificate type: ${data.isCertificateAuthority ? 'CA' : 'Normal'}`}</Text>
-		<Text>Description:</Text>
-		<Text style={{ whiteSpace: 'pre-line', wordBreak: 'break-word' }}>{data.certificateDescription || "No description was provided"}</Text>
-	</Stack>
+function InternalCertificateDisplay({ data, organizationId }: { data: z.infer<typeof schemas.CertificateModelWithId>, organizationId: string }) {
+	return <Group className="items-stretch">
+		<Stack className="flex-1">
+			<Group justify="left">
+				<Text>Certificate name: {data.certificateName}</Text>
+				<Flex wrap={"wrap"} style={{ maxWidth: 150 }}>
+					{data.tags?.map(y => <Pill key={y}>{y}</Pill>)}
+				</Flex>
+			</Group>
+			<Text>{`Certificate type: ${data.isCertificateAuthority ? 'CA' : 'Normal'}`}</Text>
+			{
+				data.certificateDescription && <Text style={{ whiteSpace: 'pre-line', wordBreak: 'break-word' }}>Description: {data.certificateDescription}</Text>
+			}
+		</Stack>
+		<DeleteButton certificateId={data.certificateId ?? ""} organizationId={organizationId} />
+	</Group>
 }
